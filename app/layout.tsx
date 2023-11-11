9893b7333681fe4bda46869130db658de3a67757
ryoon-with-wisdomtrees <ryoon.with.wisdomtrees@gmail.com>
2023-11-11 17:46:50 +0900

    heroicons를 적용한 header navBar작성

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NextAuthSessionProvider from "./provider/Provider";
+import HeaderNavbar from "./components/HeaderNavbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextAuthSessionProvider>{children}</NextAuthSessionProvider>
+        <NextAuthSessionProvider>
+          <HeaderNavbar />
+          {children}
+        </NextAuthSessionProvider>
       </body>
     </html>
   );
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: false,
 
-module.exports = nextConfig
+  images: {
+    unoptimized: true,
+    domains: ["lh3.googleusercontent.com", "maps.googleapis.com"],
+  },
+};
+
+module.exports = nextConfig;
